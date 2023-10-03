b54fd4e773459f08f1f5d55e971eaf7dc8b00600
tawheedshimul <tawheedshimul@gmail.com>
2023-10-03 13:22:23 +0600

    updta

---
+++ Control-Flow/Conditional-Statement/StateMent.js
@@ -0,0 +1,63 @@
+// If Statement:
+if (condition) {
+    // Code to execute if the condition is true
+}
+
+
+
+
+// If-Else Statement:
+if (condition) {
+    // Code to execute if the condition is true
+} else {
+    // Code to execute if the condition is false
+}
+
+
+
+// If-Else If-Else Statement:
+if (condition1) {
+    // Code to execute if condition1 is true
+} else if (condition2) {
+    // Code to execute if condition2 is true
+} else {
+    // Code to execute if no conditions are true
+}
+
+
+
+// Switch Statement:
+switch (expression) {
+    case value1:
+        // Code to execute if expression equals value1
+        break;
+    case value2:
+        // Code to execute if expression equals value2
+        break;
+    default:
+        // Code to execute if expression doesn't match any case
+}
+
+
+// Ternary (Conditional) Operator:
+
+let result1 = (condition) ? valueIfTrue : valueIfFalse;
+
+
+
+// Nullish Coalescing Operator (??):
+let result2 = someValue ?? defaultValue;
+
+
+
+// Logical AND (&&) Operator with Short-Circuiting:
+if (condition1 && condition2) {
+    // Code to execute if both condition1 and condition2 are true
+}
+
+
+
+// Logical OR (||) Operator with Short-Circuiting:
+if (condition1 || condition2) {
+    // Code to execute if either condition1 or condition2 is true
+}
