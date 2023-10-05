29aabd7149de83fdc7959185b8d0773c65b2f796
tawheedshimul <tawheedshimul@gmail.com>
2023-10-05 12:15:07 +0600

    update

--- Control-Flow/Loop/While.js
+++ Control-Flow/Loop/While.js
@@ -1,7 +1,23 @@
 // if conditon full fill while loop running , if condition not fullfill while loop stop 
 
-var i = 0
-while( i<10){
-    console.log("Tawheed")
-    i++;
+// var i = 0
+// while( i<10){
+//     console.log("Tawheed")
+//     i++;
+// }
+
+
+
+var isRunning = true;
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+
+    if (rand === 9) {
+        console.log("winner");
+        isRunning = false;
+    } else {
+        console.log("Sorry");
+    }
 }
+
+console.log(rand);
